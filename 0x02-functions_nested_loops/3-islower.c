12c7802f31e11959f7b05f4e8de7322623c1c1c7
eotobi <eotobi@gmail.com>
2022-12-15 11:53:14 -0800

    Fourth solution commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _islower - a function that checks for lowercase character
+*
+* @n: single letter input
+* 
+* Return: 1 if int n is lowercase, 0 if otherwise
+*/
+
+int _islower(int n)
+{
+	if (n >= 'a' && n <= 'z')
+		return (1);
+	else
+		return (0);
+}
