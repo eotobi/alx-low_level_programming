994c281c56d709dffb013052f00e561533a30e97
eotobi <eotobi@gmail.com>
2022-12-15 11:57:43 -0800

    Fourth solution commit updates

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,10 +4,8 @@
 * _islower - a function that checks for lowercase character
 *
 * @n: single letter input
-* 
 * Return: 1 if int n is lowercase, 0 if otherwise
 */
-
 int _islower(int n)
 {
 	if (n >= 'a' && n <= 'z')
