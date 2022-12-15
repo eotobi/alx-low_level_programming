62b5fa3900ee9ffb66b1ed889ceb959f2f1ecf36
eotobi <eotobi@gmail.com>
2022-12-15 12:46:17 -0800

    Mandatory tasks solutions commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result
+ * @one: the first number input
+ * @two: the second number input
+ * Return: one + two
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _abs - a function that computes the absolute value of an integer
+* @abn: integer input absolute number
+* Return: absolute number of abn
+*/
+int _abs(int ab)
+{
+	return (abn * ((abn > 0) - (abn < 0)));
+
+}
