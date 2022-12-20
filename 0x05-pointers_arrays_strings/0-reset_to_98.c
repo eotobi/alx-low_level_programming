9316792f2ef6b866359f443c0565d411a2deda40
eotobi <eotobi@gmail.com>
2022-12-20 10:48:05 -0800

    fiexes to 1st solution

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code 
- * Return: Always 0.
+ * reset_to_98 - Updates the value it points to 98
+ * @n: pointer to n
+ * Return: void
  */
-int main(void)
+void reset_to_98(int *n)
 {
-	int n;
+	*n = 98;
 
-	n = 402;
-	printf("n=%d\n", n);
-        reset_to_98(&n);
-	printf("n=%d\n", n);
-        return (0);
 }
+
