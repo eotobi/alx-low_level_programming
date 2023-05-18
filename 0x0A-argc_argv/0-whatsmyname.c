92955f774cefb1839a33c16cd1678f6284a1a512
eotobi <eotobi@gmail.com>
2023-05-18 02:01:42 -0700

    happy hour :)

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-    printf("%s\n", argv[0]);
+/**
+ * main - Entry point of the program
+ * @argc: Number of command line arguments
+ * @argv: Array containing the command line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+    /* Extract the program name from the provided path */
+    char *programName = strrchr(argv[0], '/');
+
+    if (programName == NULL)
+        programName = argv[0];
+    else
+        programName++;
+
+    /* Print the program name */
+    printf("%s\n", programName);
+
     return 0;
 }
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    printf("Number of arguments: %d\n", argc - 1);
+/**
+ * main - Entry point of the program
+ * @argc: Number of command line arguments
+ * @argv: Array containing the command line arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+    /* Print the number of arguments */
+    printf("%d\n", argc - 1);
+
     return 0;
 }
+
