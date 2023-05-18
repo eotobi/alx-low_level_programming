b0693a8b35e40d9f4029b62fd9357ed39ed1e42f
eotobi <51699128+eotobi@users.noreply.github.com>
2023-05-18 10:14:36 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c *.c
-ar rc liball.a *.o
-ranlib liball.a
+
+# Compile .c files into object files
+gcc -c *.c
+
+# Create static library using ar
+ar rcs liball.a *.o
+
+# Cleanup - remove the object files
+rm *.o
+
+echo "Static library liball.a created successfully."
