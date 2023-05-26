012e3d89b3c9cc6f034ef3a88a2f6ab66cce230e
eotobi <eotobi@gmail.com>
2023-05-26 03:07:55 -0700

    mandatory taks commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* print_numbers - list numbers
+* @separator: separator
+* @n: number of params
+* Return: void
+*/
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int counter = 0;
+
+	va_start(ap, n);
+
+	for (; counter < n; counter++)
+	{
+	if (counter < (n - 1)  && separator != 0)
+		printf("%d%s", va_arg(ap, int), separator);
+	else
+		printf("%d", va_arg(ap, int));
+	}
+	va_end(ap);
+	putchar('\n');
+
+}
